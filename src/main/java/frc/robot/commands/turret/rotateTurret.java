8074e031939e7081dd34b096a2e38a35470f6953
a-chahal <aaranchahal09@gmail.com>
2026-02-18 17:05:27 -0800

    remove

--- src/main/java/frc/robot/commands/turret/rotateTurret.java
+++ src/main/java/frc/robot/commands/turret/rotateTurret.java
@@ -16,11 +16,6 @@ public class rotateTurret extends Command{
     }
 
     @Override
-    public void initialize(){
-        setpoint = Degrees.of(0);
-    }
-
-    @Override
     public void execute(){
         m_turretSubsystem.moveTurret(setpoint);
     }
